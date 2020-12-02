01785d3350ffafb6f2dcf065cd36397b6f09a153
Oisin Doherty <oisin.doherty@codema.ie>
2020-12-02 14:07:11 +0000

    Updated styling and export to parquet

--- src/vo-spatial-match/spatial_transform_vo.py
+++ src/vo-spatial-match/spatial_transform_vo.py
@@ -1,21 +1,22 @@
-import pandas as pd 
-import geopandas as gpd 
+import geopandas as gpd
+import pandas as pd
+from prefect import Flow, task
 
-from prefect import Flow
-from prefect import task
 
 @task
 def _read_vo(path: str) -> gpd.GeoDataFrame:
 
     return gpd.read_parquet(path)
 
+
 @task
-def _read_geometries(path:str) -> gpd.GeoDataFrame:
+def _read_geometries(path: str) -> gpd.GeoDataFrame:
 
     return gpd.read_parquet(path)
 
-@task 
-def _read_eds(path:str) -> gpd.GeoDataFrame:
+
+@task
+def _read_eds(path: str) -> gpd.GeoDataFrame:
 
     gdf = gpd.read_file(path)
 
@@ -23,24 +24,31 @@ def _read_eds(path:str) -> gpd.GeoDataFrame:
 
 
 @task
-def _merge_vo_sa(df: pd.DataFrame, gdf: gpd.GeoDataFrame, ) -> gpd.GeoDataFrame:
+def _merge_vo_sa(df: pd.DataFrame, gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
 
     return gpd.sjoin(df, gdf, how="inner")
+
 
 @task
 def _remove_index(df: pd.DataFrame) -> pd.DataFrame:
 
-    return df.drop(['index_right'], axis=1)
+    return df.drop(["index_right"], axis=1)
+
 
 @task
-def _merge_vo_post(df: pd.DataFrame, gdf: gpd.GeoDataFrame, ) -> gpd.GeoDataFrame:
+def _merge_vo_post(
+    df: pd.DataFrame,
+    gdf: gpd.GeoDataFrame,
+) -> gpd.GeoDataFrame:
 
     return gpd.sjoin(df, gdf, how="inner")
 
+
 @task
-def _merge_vo_ed(df: pd.DataFrame, gdf: gpd.GeoDataFrame, ) -> gpd.GeoDataFrame:
+def _merge_vo_ed(df: pd.DataFrame, gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
 
     return gpd.sjoin(df, gdf, how="inner")
+
 
 @task
 def _extract_columns(df: pd.DataFrame, equiv: list) -> pd.DataFrame:
@@ -48,7 +56,13 @@ def _extract_columns(df: pd.DataFrame, equiv: list) -> pd.DataFrame:
     return df[df[equiv]]
 
 
-with Flow ("Assign SA, ED and Postcode to each VO building") as flow:
+@task
+def _df_to_parquet(df: pd.DataFrame, path: str) -> pd.DataFrame:
+
+    return df.to_parquet(path)
+
+
+with Flow("Assign SA, ED and Postcode to each VO building") as flow:
 
     vo = _read_vo("data/vo.parquet")
     sa = _read_geometries("data/sa_geometries.parquet")
@@ -59,8 +73,20 @@ with Flow ("Assign SA, ED and Postcode to each VO building") as flow:
     vo_sa_post = _merge_vo_post(vo_sa_dropped, postcode)
     sa_post_dropped = _remove_index(vo_sa_post)
     vo_final = _merge_vo_ed(sa_post_dropped, ed)
-    vo_extracted = _extract_columns('Address', 'Uses', 'benchmark', 'typical_fossil_fuel','typical_electricity', 'typical_fossil_fuel_demand','typical_electricity_demand', 'geometry', 'small_area', 'postcodes', 'CSOED')
-
+    vo_extracted = _extract_columns(
+        "Address",
+        "Uses",
+        "benchmark",
+        "typical_fossil_fuel",
+        "typical_electricity",
+        "typical_fossil_fuel_demand",
+        "typical_electricity_demand",
+        "geometry",
+        "small_area",
+        "postcodes",
+        "CSOED",
+    )
+    vo_output = _df_to_parquet(vo_extracted, "data/vo_spatial.parquet")
 
 
 if __name__ == "__main__":
