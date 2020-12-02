9b2332584bbfcddd931d18c74a5cba5e34a164fb
Oisin Doherty <oisin.doherty@codema.ie>
2020-12-02 11:40:14 +0000

    Added ED shapefiles and added prefect flow

--- src/vo-spatial-match/spatial_transform_vo.py
+++ src/vo-spatial-match/spatial_transform_vo.py
@@ -2,6 +2,37 @@ import pandas as pd
 import geopandas as gpd 
 
 from prefect import Flow
-from prefect import Parameter
-from prefect import Task
 from prefect import task
+
+@task
+def _read_vo(path: str) -> gpd.GeoDataFrame:
+
+    return gpd.read_parquet(path)
+
+@task
+def _read_geometries(path:str) -> gpd.GeoDataFrame:
+
+    return gpd.read_parquet(path)
+
+@task
+def _merge_vo_sa(df: pd.DataFrame, gdf: gpd.GeoDataFrame, ) -> gpd.GeoDataFrame:
+
+    return gpd.sjoin(df, gdf, how="inner")
+
+@task
+def _remove_index(df: pd.DataFrame) -> pd.DataFrame:
+
+    return df.drop(['index_right'], axis=1)
+
+
+with Flow ("Assign SA, ED and Postcode to each VO building") as flow:
+
+    vo = _read_vo("data/vo.parquet")
+    sa = _read_geometries("data/sa_geometries.parquet")
+    postcode = _read_geometries("data/dublin_postcodes.parquet")
+    vo_sa = _merge_vo_sa(vo, sa)
+    vo_sa_dropped = _remove_index(vo_sa)
+
+
+if __name__ == "__main__":
+    state = flow.run()
