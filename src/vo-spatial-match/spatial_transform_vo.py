31f0057151ce2828456de6569dc861562dce0f50
Oisin Doherty <oisin.doherty@codema.ie>
2020-12-02 11:59:57 +0000

    Merged vo spatially using gpd sjoin

--- src/vo-spatial-match/spatial_transform_vo.py
+++ src/vo-spatial-match/spatial_transform_vo.py
@@ -17,7 +17,10 @@ def _read_geometries(path:str) -> gpd.GeoDataFrame:
 @task 
 def _read_eds(path:str) -> gpd.GeoDataFrame:
 
-    return gpd.read_file(path)
+    gdf = gpd.read_file(path)
+
+    return gdf.to_crs(epsg="4326")
+
 
 @task
 def _merge_vo_sa(df: pd.DataFrame, gdf: gpd.GeoDataFrame, ) -> gpd.GeoDataFrame:
@@ -29,7 +32,15 @@ def _remove_index(df: pd.DataFrame) -> pd.DataFrame:
 
     return df.drop(['index_right'], axis=1)
 
+@task
+def _merge_vo_post(df: pd.DataFrame, gdf: gpd.GeoDataFrame, ) -> gpd.GeoDataFrame:
 
+    return gpd.sjoin(df, gdf, how="inner")
+
+@task
+def _merge_vo_ed(df: pd.DataFrame, gdf: gpd.GeoDataFrame, ) -> gpd.GeoDataFrame:
+
+    return gpd.sjoin(df, gdf, how="inner")
 
 
 with Flow ("Assign SA, ED and Postcode to each VO building") as flow:
@@ -40,6 +51,10 @@ with Flow ("Assign SA, ED and Postcode to each VO building") as flow:
     ed = _read_eds("data/ed/electoral_divisions.shp")
     vo_sa = _merge_vo_sa(vo, sa)
     vo_sa_dropped = _remove_index(vo_sa)
+    vo_sa_post = _merge_vo_post(vo_sa_dropped, postcode)
+    sa_post_dropped = _remove_index(vo_sa_post)
+    vo_final = _merge_vo_ed(sa_post_dropped, ed)
+
 
 
 if __name__ == "__main__":
